32749a12780e99163f49581dd62a4d8551934f2f
Norman Hooper <nhooper@dimagi.com>
2026-04-26 12:30:04 -0400

    Add discover function

--- src/assertions/__init__.py
+++ src/assertions/__init__.py
@@ -1,3 +1,4 @@
+from assertions._discover import discover
 from assertions._markers import test
 
-__all__ = ['test']
+__all__ = ['discover', 'test']
---
+++ src/assertions/_discover.py
@@ -0,0 +1,12 @@
+from types import ModuleType
+from typing import Callable
+
+from assertions._markers import TEST_MARKER
+
+
+def discover(module: ModuleType) -> list[Callable]:
+    return [
+        value
+        for value in vars(module).values()
+        if callable(value) and getattr(value, TEST_MARKER, False) is True
+    ]
